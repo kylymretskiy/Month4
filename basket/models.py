dcf823168a1cfd9d95d99bbacad44378542b21b2
kylymretskiy <kylaorozaliev@gmail.com>
2025-03-18 16:40:56 +0600

    HW4

---
+++ basket/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from books.models import Books
+
+class TodoList(models.Model):
+    STATUS = (
+        ('✅', '✅'),
+        ('❌', '❌'),
+    )
+    title = models.CharField(max_length=100)
+    choice_books = models.ForeignKey(Books, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20,choices=STATUS, default='❌')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
