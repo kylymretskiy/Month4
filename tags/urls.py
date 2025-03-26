011676eeb2f951551f7ce13c4802ca345718a32a
kylymretskiy <kylaorozaliev@gmail.com>
2025-03-26 11:51:18 +0600

    HW6

--- tags/urls.py
+++ tags/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('all_tags_films/', views.AllCategoryBooksView.as_view(), name='all'),
     path('teen_tags_films/', views.TeenBooksView.as_view(), name='teen'),
     path('kids_tags_films/', views.KidsBooksView.as_view(), name='kids'),
+
 ]
